031aafb379d00161662e74cf1cf43f281754d8d5
Anth0nyPereira <anthonypereira@ua.pt>
2021-08-17 19:51:48 +0100

    Another try for the rocket's head - this time, using an extrude

--- scene.js
+++ scene.js
@@ -83,20 +83,19 @@ function addLineSegment(geometry, color) {
     return line;
 }
 
-function createBird() {
-    var radius = 2;  
-    var detail = 5;  
-    var geometry = new THREE.TetrahedronGeometry(radius, detail);
-    var material = new THREE.MeshBasicMaterial({color: 0x0000ff});
-    var mesh = new THREE.Mesh(geometry, material);
-    return mesh;
-}
-
 function createRocketHead() {
-    var radius = 3, height = 3, radialSegments = 32;  
-    var geometry = new THREE.ConeGeometry(radius, height, radialSegments);
+    var shape = new THREE.Shape();
+    shape.moveTo(2, 0);
+    shape.bezierCurveTo(1.5, 1, 2.5, 2, 0, 3);
+    shape.bezierCurveTo(-2.5, 2, -1.5, 1, -2, 0);
+    shape.lineTo(2, 0);
+
+    var extrudeSettings = {steps: 2, depth: 0.2, bevelEnabled: true, bevelThickness: 1, bevelSize: 1, bevelSegments: 10};
+
+    var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
     var material = new THREE.MeshBasicMaterial({color: 0xff0000});
-    var mesh = new THREE.Mesh(geometry, material);
+    var mesh = new THREE.Mesh(geometry, material);    
+    mesh.scale.y = 1.3;
     return mesh;
 }
 
@@ -133,30 +132,6 @@ function createRocket() {
     return rocket;
 }
 
-// function that will return an obstacle already made of 2 stars that only has the centering position and the height of the hole between the two poles
-function createObstacle(centerPosition, holeHeight) {
-
-    var obstacle = new THREE.Group();
-    
-    // first, let's create the star at the bottom
-    var star1Height = centerPosition - holeHeight/2;
-    var star1 = createStar(star1Height);
-    star1.position.y = star1Height/2;
-
-    // now, the second star at the top
-    // let's define a maximum value for the height of an obstacle (conjunction of two stars) --> 500 of height
-
-    var star2Height = 500 - holeHeight - star1Height;
-    var star2 = createStar(star2Height);
-    star2.position.y = star2Height/2 + holeHeight + star1Height;
-
-    obstacle.add(star1);
-    obstacle.add(star2);
-
-    sceneElements.obstaclesGroup.push(obstacle);
-    return obstacle;
-}
-
 // function to return a random integer between two values
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
@@ -182,17 +157,10 @@ function load3DObjects(sceneGraph) {
     // Set shadow property
     planeObject.receiveShadow = true;
 
-    // Create bird
-    var bird = createBird();
-    var target = new THREE.Vector3();
-    bird.position.set(0, sceneElements.camera.getWorldPosition(target).y, sceneElements.camera.getWorldPosition(target).z);
-    bird.name = "bird";
-    sceneElements.sceneGraph.add(bird);
-
     // FOR TESTING PURPOSES
     // create rocket edge
     var rocket = createRocket();
-    rocket.position.set(0, sceneElements.camera.getWorldPosition(target).y, sceneElements.camera.getWorldPosition(target).z);
+    rocket.position.set(0, sceneElements.camera.position.y, sceneElements.camera.position.z);
     sceneElements.sceneGraph.add(rocket);
 
     //var obstacle1 = createObstacle(40, 20);
@@ -225,93 +193,8 @@ var obstaclePosition = sceneElements.camera.position.z - 100;
 var obstacleID = 1;
 
 var gameOver = true;
-function birdIntersectsObstacle() {
-    var bird = sceneElements.sceneGraph.getObjectByName("bird");
-    var raycaster = new THREE.Raycaster();
-    raycaster.set(new THREE.Vector3(bird.position.x, bird.position.y, bird.position.z), new THREE.Vector3(-1, 0, 0));
-    if (sceneElements.obstaclesGroup.length > 0) {
-        for (var i=parseInt(sceneElements.obstaclesGroup[0].name); i<parseInt(sceneElements.obstaclesGroup[sceneElements.obstaclesGroup.length - 1].name); i++) {
-            var intersects = raycaster.intersectObjects(sceneElements.obstaclesGroup[0].children, true); // true -> means recursively checking its children
-            if (intersects.length > 0 && gameOver) {
-                alert("GAME OVER!");
-                gameOver = false;
-            }
-        }
-    }  
-}
 
-function removePreviousObstacles() {
-    var bird = sceneElements.sceneGraph.getObjectByName("bird");
-    for (var i=0; i<sceneElements.obstaclesGroup.length; i++) {
-        var oldObstacle = sceneElements.obstaclesGroup[0];
-        if (Math.abs(bird.position.z - oldObstacle.position.z) > 200) {
-            //console.log(oldObstacle);
-            sceneElements.sceneGraph.remove(oldObstacle);
-            sceneElements.obstaclesGroup.splice(oldObstacle, 1); 
-        }
-    }
-}
 function computeFrame(time) {
-
-    birdIntersectsObstacle();
-    removePreviousObstacles();
-
-    //sceneElements.camera.position.z -= 0.5;
-    sceneElements.control.target = new THREE.Vector3(-Math.pow(10, 10), 0, 0);
-
-    var bird = sceneElements.sceneGraph.getObjectByName("bird");
-    bird.position.z -= 0.5;
-    
-    var target = new THREE.Vector3();
-    /*
-    console.log(sceneElements.camera.getWorldPosition(target));
-    console.log(sceneElements.camera);
-    */
-
-    // Adding obstacles loop
-    var centerPosition = randomIntFromInterval(20, 60);
-    if (sceneElements.centerPositionValues.length >= 2) {
-        while (Math.abs(sceneElements.centerPositionValues[sceneElements.centerPositionValues.length - 1] - centerPosition) > 20) {
-            centerPosition = randomIntFromInterval(20, 60);
-        }
-    }
-    sceneElements.centerPositionValues.push(centerPosition);
-    var obstacle = createObstacle(centerPosition, 10);
-    obstacle.position.z = obstaclePosition;
-    obstaclePosition -= 50;
-    sceneElements.sceneGraph.add(obstacle);
-    obstacle.name = obstacleID;
-    obstacleID += 1;
-    
-    // Position-y of bird increases when pressing space
-    
-    if (space) {
-        var initialPosition = bird.position.y;
-        if (sceneElements.birdInitialPosition.length > 0 && birdFlag) {
-            if (Math.abs(sceneElements.birdInitialPosition[0] - initialPosition) >= 10) {
-                //deltaBirdY *= -1; 
-                sceneElements.birdInitialPosition = [];
-                birdFlag = false;
-                //console.log(birdFlag);
-            }
-        }
-
-        if (birdFlag) {
-            sceneElements.birdInitialPosition.push(initialPosition);
-            //console.log(sceneElements.birdInitialPosition);
-            bird.position.y += 6*deltaBirdY;
-        } else if (!birdFlag) {
-            bird.position.y -= 3*deltaBirdY;
-        }
-        
-
-    } else if (!space) {
-        bird.position.y -= 3*deltaBirdY;
-        birdFlag = true;
-        //console.log(birdFlag);
-    }
-
-    
 
     // Rendering
     helper.render(sceneElements);
